b6ffe1104a2f428494d98524ae1c723b411ba0b1
Lim Hyung Woo <jeota0424@pusan.ac.kr>
2019-06-29 13:54:38 +0900

    Add Naver Map
    1. add gradle ( navermap )
    2. add Fragment for naver map
    3. add meta-data in manifests

--- app/src/main/java/com/pb/hw/MainActivity.kt
+++ app/src/main/java/com/pb/hw/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var name : String = "임형우"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
